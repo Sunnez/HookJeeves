8dc9d990f5891666a7b30bf3220b020a66845ca5
Maciej Gaciarz <m.gaciarz93@gmail.com>
2019-01-23 23:53:28 +0100

    fixed points, and better drawing

--- HJ.py
+++ HJ.py
@@ -2,7 +2,6 @@ from tom178 import hooke
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from scipy import interpolate
 import adjustText as at
 #   Input, integer NVARS, the number of spatial dimensions.
 #
@@ -49,12 +48,12 @@ def functionValue(x, n):
     #value = 2 * x[0]**2 + 2 * x[0] * x[1] + 2 * x[1]**2 - 6 * x[0]
     return value
 
-step = 0.1   # krok
+step = 1   # krok
 nvars = 2
-startpt = [0,0]
+startpt = [2, 3]
 itermax = 5000
-rho = 0.5
-eps = 1.0E-04
+rho = 0.5               # a user-supplied convergence parameter which should be set to a value between 0.0 and 1.0.
+eps = 1.0E-06           # the criterion for halting the search for a minimum.
 x0 = startpt.copy()
 d1 = np.array([1, 0])
 d2 = np.array([0, 1])
@@ -62,10 +61,11 @@ a = x0
 
 showsteps = False
 showStartAndEnd = False
+optimizeGraph = False
 
 
 #def hooke(nvars, startpt, rho, eps, itermax, f):
-it, endpt, points = hooke(2, startpt, rho, eps, 5000, functionValue)
+it, endpt, points = hooke(2, startpt, rho, eps, 1000, functionValue)
 
 
 
@@ -80,7 +80,7 @@ for i in range(len(points)):
 print(len(xs))
 plt.scatter(xs, ys, marker = 'x')
 
-# starting point - blue
+# starting point - yellow
 plt.scatter(xs[0], ys[0], c='yellow', s=15, marker = 'x')
 
 # end point red
@@ -95,8 +95,9 @@ plt.title("Hook - Jeeves Method")
 plt.xlabel("X")
 plt.ylabel("Y")
 
-#showsteps = True
-showStartAndEnd = True
+showsteps = True
+#showStartAndEnd = True
+
 if showsteps:
     texts = [plt.text(xs[i], ys[i], 'Step '+ str(i+1) , ha='center', va='center') for i in range(len(xs))]
     at.adjust_text(texts, xs, ys, arrowprops=dict(arrowstyle='->', color='black'))
@@ -108,9 +109,17 @@ if showStartAndEnd:
     #adjust texts so they dont overlap
     at.adjust_text(firstPointText, xs, ys, arrowprops=dict(arrowstyle='-', color='black'))
     at.adjust_text(endPointText, endpt, endpt, arrowprops=dict(arrowstyle='-', color='black'))
+
+def OptimizeGraph():
+    xmin = min(x for x in xs)
+    xmax = max(x for x in xs)
+    ymin = min(y for y in ys)
+    ymax = max(y for y in ys)
+    plt.axis([xmin, xmax, ymin, ymax])
+
 plt.show()
 print('')
-print('  Number of iterations taken = %d' % (it))
+print('Number of iterations taken = %d' % (it))
 print('--------------------------------------------')
-print('endpts:\n')
+print('Last point - minimum equals:\n')
 print(endpt)
